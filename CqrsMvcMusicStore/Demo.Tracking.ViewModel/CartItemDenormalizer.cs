762e59d2473f0075f05818735fec12729a9ad5cc
Eric De Carufel <decarufe@users.noreply.github.com>
2012-04-22 02:19:17 +0000

    Tracking

--- CqrsMvcMusicStore/Demo.Tracking.ViewModel/CartItemDenormalizer.cs
+++ CqrsMvcMusicStore/Demo.Tracking.ViewModel/CartItemDenormalizer.cs
@@ -4,27 +4,29 @@ using System.Linq;
 using System.Text;
 using Demo.Events;
 using SimpleCqrs.Eventing;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace Demo.Tracking.ViewModel
 {
    public class CartItemDenormalizer : IHandleDomainEvents<CartItemRemovedEvent>
    {
-      private object _locker = new object();
-      private static int _removedCount = 0;
-
       public void Handle(CartItemRemovedEvent domainEvent)
       {
-         lock (_locker)
+         using (var connexion = new SqlConnection(ConfigurationManager.ConnectionStrings["CQRS"].ConnectionString))
          {
-            _removedCount++;
-         }
+            connexion.Open();
 
+            using (var command = connexion.CreateCommand())
+            {
+               command.CommandText = "UPDATE Retrait SET Count = Count + 1 WHERE ID = @ID IF @@ROWCOUNT = 0 INSERT INTO Retrait (Id, Count) values(@ID, 0)";
+               command.Parameters.Add("@ID", SqlDbType.Int).Value = domainEvent.Id;
 
-         if (_removedCount == 5)
-         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Envoyer un email au directeur des ventes, il est temps d'offrir un rabais surprise!!!");
-            Console.ResetColor();
+               command.ExecuteNonQuery();
+            }
+
+            connexion.Close();
          }
       }
    }
