48004c2515bb91b31ee36bc12ebe760d3fb4c465
lavignep007_cp <SND\lavignep007_cp>
2012-04-22 03:25:47 +0000

    Ajout de la page des stats

---
+++ CqrsMvcMusicStore/MvcMusicStore/ViewModels/CartItemRemoveView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+public class CartItemRemoveView
+{
+   public List<ShoppingCartRemoveItem> Items { get; set; }
+}
+
+public class ShoppingCartRemoveItem
+{
+   public string AlbumName { get; set; }
+   public int AlbumId { get; set; }
+   public int Count { get; set; }
+}
